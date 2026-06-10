4ff05e8f42cf9222a3842ecb43951512e84a2da9
Jhon Doe <john.doe@email.com>
2026-06-10 13:43:18 +0200

    etape 3 + etape 4

--- 02-string-calculator/src/string-calculator.test.ts
+++ 02-string-calculator/src/string-calculator.test.ts
@@ -1,13 +1,27 @@
 import { assertEquals } from "jsr:@std/assert"
-import { add } from "./string-calculator.ts"
+import { add, add2, add3 } from "./string-calculator.ts"
 
-//Étape 1 - chaîne vide retourne 0
+//Étape 1 - chaîne vide retourne 0 (red)
 Deno.test("empty string returns 0", () => {
   assertEquals(add(""), 0)
 })
 
-//Étape 2 - un nombre retourne lui-même
+//Étape 2 - un nombre retourne lui-même (red)
 
 Deno.test("single number returns itself", () => {
-  assertEquals(add("1"), 1)
+  assertEquals(add2("1"), 1)
 })
+
+//Étape 3 - deux nombres séparés par une virgule (red)
+
+Deno.test("two numbers separated by comma", () => {
+ assertEquals(add3("1,2"), 3)
+})
+
+//Étape 4 - plusieurs nombres (red)
+Deno.test("multiple numbers", () => {
+ assertEquals(add4("1,2,3,4"), 10)
+})
+
+
+
--- 02-string-calculator/src/string-calculator.ts
+++ 02-string-calculator/src/string-calculator.ts
@@ -1,15 +1,33 @@
-//v1 Étape 1 - chaîne vide retourne 0
+//v1 Étape 1 - chaîne vide retourne 0 (green)
 export function add(numbers: string): number {
   if (numbers === "") return 0
 
   throw new Error("Not implemented")
 }
 
-//Étape 2 - un nombre retourne lui-même
+//Étape 2 - un nombre retourne lui-même (green)
 
 export function add2(numbers: string): number {
   if (numbers === "") return 0
 
   return Number(numbers)
 }
+
+//Étape 3 - deux nombres séparés par une virgule (green)
+export function add3(numbers: string): number {
+ if (numbers === "") return 0
+
+ const values = numbers.split(",")
+
+ return values.reduce((sum, value) => sum + Number(value), 0)
+}
+//Étape 4 - plusieurs nombres (green)
+export function add4(numbers: string): number {
+ if (numbers === "") return 0
+
+ const values = numbers.split(",")
+
+ return values.reduce((sum, value) => sum + Number(value), 0)
+}
+
 
