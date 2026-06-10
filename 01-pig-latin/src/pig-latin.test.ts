5fd02cc604450ae8a44c82f292e9e9e69b4176e2
Nahim Mehadji <nahim.mehadji2@univ-lorraine.fr>
2026-06-10 14:02:48 +0200

    Ajout des tests ear et igloo

--- 01-pig-latin/src/pig-latin.test.ts
+++ 01-pig-latin/src/pig-latin.test.ts
@@ -4,3 +4,11 @@ import { pigLatin } from "./pig-latin.ts"
 Deno.test("apple devient appleay", () => {
   assertEquals(pigLatin("apple"), "appleay")
 })
+
+Deno.test("ear devient earay", () => {
+  assertEquals(pigLatin("ear"), "earay")
+})
+
+Deno.test("igloo devient iglooay", () => {
+  assertEquals(pigLatin("igloo"), "iglooay")
+})
