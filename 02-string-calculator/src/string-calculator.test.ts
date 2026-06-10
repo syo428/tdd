75173f7b3e17a42b87a5b40b6320ef966d5f1439
Jhon Doe <john.doe@email.com>
2026-06-10 13:38:13 +0200

    etape 1 + etape 2

--- 02-string-calculator/src/string-calculator.test.ts
+++ 02-string-calculator/src/string-calculator.test.ts
@@ -1,3 +1,13 @@
-import { assertEquals, assertThrows } from "jsr:@std/assert"
+import { assertEquals } from "jsr:@std/assert"
 import { add } from "./string-calculator.ts"
 
+//Étape 1 - chaîne vide retourne 0
+Deno.test("empty string returns 0", () => {
+  assertEquals(add(""), 0)
+})
+
+//Étape 2 - un nombre retourne lui-même
+
+Deno.test("single number returns itself", () => {
+  assertEquals(add("1"), 1)
+})
--- 02-string-calculator/src/string-calculator.ts
+++ 02-string-calculator/src/string-calculator.ts
@@ -1,3 +1,15 @@
+//v1 Étape 1 - chaîne vide retourne 0
 export function add(numbers: string): number {
+  if (numbers === "") return 0
+
   throw new Error("Not implemented")
 }
+
+//Étape 2 - un nombre retourne lui-même
+
+export function add2(numbers: string): number {
+  if (numbers === "") return 0
+
+  return Number(numbers)
+}
+
